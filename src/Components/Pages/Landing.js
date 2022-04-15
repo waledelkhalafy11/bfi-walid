060cb2edb800a56cfb1d97fed172fca8212ece62
Younes15298 <mohamed15298a7@gmail.com>
2022-04-15 18:58:05 +0200

    last update

--- src/Components/Pages/Landing.js
+++ src/Components/Pages/Landing.js
@@ -1,12 +1,13 @@
 import FixedNavbar from "../Layouts/Navbar";
 import Hero from "../Layouts/Hero";
 import Footer from "../Layouts/Footer";
-
+import Blog from "../Layouts/Blogs";
 const Landing = () => {
   return (
     <>
-     <FixedNavbar/>
-     <Hero/>
+    <FixedNavbar/>
+    <Hero/>
+    <Blog/>
     <Footer/>
     </>
   );
