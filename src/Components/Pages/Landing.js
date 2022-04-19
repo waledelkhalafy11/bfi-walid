9726c442fa4a6512a9f11fa92e7eba086d1cc70e
Alymansour94 <Alymansour3@yahoo.com>
2022-04-20 01:57:18 +0200

    Adding unit page with share to social

--- src/Components/Pages/Landing.js
+++ src/Components/Pages/Landing.js
@@ -1,7 +1,9 @@
 import Hero from "../Layouts/Hero";
 import Footer from "../Layouts/Footer";
 import Blog from "../Layouts/Blogs";
-import Imgs from '../Layouts/Imgs';
+import Imgs from '../Layouts/Imgs'
+
+import UnitPage from "./UnitPage";
 
 const Landing = () => {
   return (
@@ -9,6 +11,7 @@ const Landing = () => {
     <Hero/>
     <Blog/>
     <Imgs/>
+    <UnitPage/>
     <Footer/>
     </>
   );
