b5b2187b573474ba0a3092aa98e37bcfec10f06e
Mohamed ShamsEldeen <mmm.emad64@gmail.com>
2022-04-20 04:57:08 +0200

    add new Component why us

--- src/Components/Pages/Landing.js
+++ src/Components/Pages/Landing.js
@@ -2,13 +2,14 @@ import Hero from "../Layouts/Hero";
 import Footer from "../Layouts/Footer";
 import Blog from "../Layouts/Blogs";
 import Imgs from '../Layouts/Imgs'
-
+import WhyUs from '../Layouts/WhyUs';
 import UnitPage from "./UnitPage";
 
 const Landing = () => {
   return (
     <>
     <Hero/>
+    <WhyUs />
     <Blog/>
     <Imgs/>
     <UnitPage/>
