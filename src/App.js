c4ada9706a2b1af13717f71df15efca938e03cee
Younes15298 <mohamed15298a7@gmail.com>
2022-04-22 17:16:57 +0200

    add

--- src/App.js
+++ src/App.js
@@ -4,11 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import FixedNavbar from "./Components/Layouts/Navbar";
 import Landing from "./Components/Pages/Landing";
 import Search from "./Components/Pages/SearchPage";
-<<<<<<< HEAD
-=======
 import UnitPage from "./Components/Pages/UnitPage";
 import ContactUs from "./Components/Layouts/ContactUs";
->>>>>>> 1a92ecf3f9fd3fc62aad381c4cc74eb4c4ccf90a
 
 function App() {
 
@@ -19,11 +16,8 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Landing />} />
           <Route exact path="/search" element={<Search />} />
-<<<<<<< HEAD
-=======
           <Route exact path="/unit" element={<UnitPage />} />
           
->>>>>>> 1a92ecf3f9fd3fc62aad381c4cc74eb4c4ccf90a
         </Routes>
     </div>
       </Router>
