1dfa8708003c979b874fdd3697df287a15a3a24f
waledelkhalafy11 <waledelkhalafy@gmail.com>
2022-05-25 21:34:44 +0200

    search filter

--- src/Components/Pages/SearchPage.js
+++ src/Components/Pages/SearchPage.js
@@ -1,19 +1,31 @@
 import GlMap from "../Layouts/GlMap";
+import SearchIcon from "@mui/icons-material/Search";
 import SearchFilter from "../Layouts/SearchFilter";
 import PlacesCard from "../Layouts/PlacesCard";
 import { ApiContext } from "../../ApiContext";
-import { Container } from "react-bootstrap";
 import { useEffect, useState, useContext } from "react";
 import SwipeableEdgeDrawer from "../Layouts/Swibable";
 import { createContext } from "react";
 import axios from "axios";
+import {
+  Form,
+  Button,
+  Container,
+  ButtonGroup,
+  ToggleButton,
+} from "react-bootstrap";
 export const SelectionContext = createContext(null);
 
 const Search = () => {
   const dataApi = useContext(ApiContext);
   const [unitsData, setunitsData] = useState(dataApi);
   const [allLocations, setLocations] = useState([]);
-  const [categories, setCategories] = useState(['Appartment' , 'Villa' , 'House' , 'Office']);
+  const [categories, setCategories] = useState([
+    "Appartment",
+    "Villa",
+    "House",
+    "Office",
+  ]);
   const [cities, setCities] = useState([]);
   const [selectedFilters, setSelectedFilters] = useState({
     region: null,
@@ -36,15 +48,10 @@ const Search = () => {
     return allregions.indexOf(c) === index;
   });
 
-  let getRegionCategories = dataApi.map((unit)=>{
-    
-     
-      ;
-    })
+  let getRegionCategories = dataApi.map((unit) => {});
 
   useEffect(() => {
     GetRequest();
-   
   }, []);
 
   let majorFilter = (item) => {
@@ -57,20 +64,18 @@ const Search = () => {
     }
   };
   let catFilter = (item) => {
-    if(item.location[0].region_name == selectedFilters.region){
+    if (item.location[0].region_name == selectedFilters.region) {
       return item;
-     }
+    }
   };
 
-let itmscat=dataApi.filter(catFilter);
-let cats = itmscat.map((unit)=>{
-  return unit.unit.category_name
-
-})
-let allCats = cats.filter((c, index) => {
-  return cats.indexOf(c) === index;
-});
-
+  let itmscat = dataApi.filter(catFilter);
+  let cats = itmscat.map((unit) => {
+    return unit.unit.category_name;
+  });
+  let allCats = cats.filter((c, index) => {
+    return cats.indexOf(c) === index;
+  });
 
   let mapData = dataApi.map((itm) => {
     return {
@@ -79,11 +84,7 @@ let allCats = cats.filter((c, index) => {
     };
   });
 
- 
-
-
   let cardDara = dataApi.map((itm) => {
-    
     return (
       <PlacesCard
         key={itm.unit.id}
@@ -134,6 +135,11 @@ let allCats = cats.filter((c, index) => {
 
   useEffect(() => {
     setunitsData(unitsData.filter(majorFilter));
+
+    dataApi.map((itm) => {
+      if (selectedFilters.region == itm.unit.region_category) {
+      }
+    });
     console.log(allCats);
   }, [selectedFilters]);
 
@@ -143,7 +149,7 @@ let allCats = cats.filter((c, index) => {
         <div className="bg-[#f2f2f2] md:h-[100vh]">
           <Container className="absolute">
             <SearchFilter
-            categories={categories}
+              categories={categories}
               regions={regions}
               sendDataToParent={sendDataToParent}
             />
@@ -164,11 +170,75 @@ let allCats = cats.filter((c, index) => {
       ) : (
         <div className="bg-[#f2f2f2] md:h-[100vh]">
           <Container className="">
-            <SearchFilter
-            categoriess={categories}
-              regions={regions}
-              sendDataToParent={sendDataToParent}
-            />
+            <Container className="static top px-[10%] p-[2%] md:px-[5%] ">
+              <Container className="bg-white border-2 p-2 md:h-[80px] rounded-3xl md:rounded-3xl mt-[20%] md:mt-[10%] flex flex-col md:flex-row">
+                <>
+                  {" "}
+                  <div className="md:w-[25%] flex mx-2 flex-col">
+                    <Form.Label className="md:ml-4 text-xl text-left">
+                      Region
+                    </Form.Label>
+                    <Form.Select
+                      className="md:ml-2 w-[100%] border-none"
+                      size="sm"
+                      onChange={(e) => sendDataToParent(e.target.value, 1)}
+                    >
+                      <option disabled selected>
+                        Select a Region
+                      </option>
+                      <option value="all">All</option>
+                      {regions.map((region) => {
+                        return <option value={region}>{region}</option>;
+                      })}
+                    </Form.Select>
+                  </div>
+                  <div className="md:w-[25%] flex mx-2 flex-col">
+                    <Form.Label className="md:ml-4 text-xl text-left">
+                      Category
+                    </Form.Label>
+                    <Form.Select
+                      className="md:ml-2 w-[100%] border-none ay7aga"
+                      size="sm"
+                    >
+                      <option disabled selected>
+                        Select a Category
+                      </option>
+                      <option value="all">All</option>
+                      {/* {categoriess.map(ii=>{
+                     return(<option value={ii} >
+                     {ii}
+                   </option>)
+                    })} */}
+                    </Form.Select>
+                  </div>
+                  <div className="md:w-[25%] flex mx-2 flex-col">
+                    <Form.Label className="md:ml-4 text-xl text-left">
+                      City
+                    </Form.Label>
+                    <Form.Select
+                      className="md:ml-2 w-[100%] border-none"
+                      size="sm"
+                    >
+                      <option disabled selected>
+                        Select City
+                      </option>
+                      <option value="all">All</option>
+                      <option>Large select</option>
+                      <option>Large select</option>
+                    </Form.Select>
+                  </div>
+                  <div className="md:w-[25%] p-2 flex mx-2 flex-col">
+                    <Button
+                      className="md:ml-2 bg-[#45b6ca] rounded-full w-[100%] border-none"
+                      size="lg"
+                    >
+                      <SearchIcon />
+                      Search
+                    </Button>
+                  </div>
+                </>
+              </Container>
+            </Container>
           </Container>{" "}
           <Container
             fluid
@@ -184,7 +254,7 @@ let allCats = cats.filter((c, index) => {
               fluid
               className="p-0 mapps g:h-[70vh] w-[100%] md:w-[70%] bg-black "
             >
-              {/* <GlMap data={mapData} /> */}
+              <GlMap data={mapData} />
             </Container>
           </Container>
         </div>
