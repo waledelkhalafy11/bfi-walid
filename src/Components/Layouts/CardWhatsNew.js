1e2206f3e97cd8fd7f0ba437c6c40c2bbd58006b
Younes15298 <mohamed15298a7@gmail.com>
2022-05-28 17:29:58 +0200

    read moore and less

--- src/Components/Layouts/CardWhatsNew.js
+++ src/Components/Layouts/CardWhatsNew.js
@@ -1,5 +1,5 @@
 import react from "react";
-
+import ReactReadMoreReadLess from "react-read-more-read-less";
 
 function CardWhatsNew (props){
     let filterDate = props.date;
@@ -33,10 +33,21 @@ if(filterDate.length > 10 ) filterDate = filterDate.substring(0,10);
                         {props.title}
                         </a>
                     </h5>
-                    <p className="card-text text-[16px] leading[1.5] text-left">
-                        {props.description}
-                    </p>
+             
+                    <p className="card-text text-[16px] leading[1.5] w-full text-left">
+                    <ReactReadMoreReadLess
+                  charLimit={100}
+                  readMoreText={<button  className="text-[#45b6ca]    font-bold  hover:opacity-80"></button>}
+                  readLessText={<button  className="text-[#45b6ca]    font-bold  hover:opacity-80"></button>}
+                  readMoreClassName="read-more-less--more"
+                  readLessClassName="read-more-less--less"
+                >
+                     {props.description} 
+                </ReactReadMoreReadLess>
 
+             
+                    </p>
+                 
                     <a
                         className="blogButton absolute   right-[10px]  bottom-[30px] text-[14px] font-normal no-underline text-[#45b6ca] py-[5px] px-[16px] border-2 border-[#45b6ca]  hover:bg-[#45b6ca] hover:text-white"
                         href=""
